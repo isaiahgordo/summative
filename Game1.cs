3c06780efecedcc454c55d82787f1258f123b888
Isaiah Gordon <GordIsai857@GoTVDSB.ca>
2022-11-30 09:38:53 -0500

    done pics

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
 
 namespace summative
 {
@@ -8,6 +10,16 @@ namespace summative
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        List<Texture2D> myCats;
+        Texture2D binkaT, beanT, saraT, shebT,introCatT;
+        Rectangle myCatR,catsR;
+        Random r;
+        List<int> x,y;
+        int gPH, gPW;
+        Screen screen;
+        MouseState mouseState;
+        SpriteFont font;
+        Vector2 v;
         enum Screen
         {
             Intro,
@@ -24,22 +36,47 @@ namespace summative
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            gPH =(_graphics.PreferredBackBufferHeight);
+            gPW =(_graphics.PreferredBackBufferWidth);
+            y =new List<int> {gPH};
+            x=new List<int> {gPW};            
+            
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            binkaT = Content.Load<Texture2D>("Binka");
+            beanT = Content.Load<Texture2D>("Bean");
+            saraT = Content.Load<Texture2D>("Sara");
+            shebT = Content.Load<Texture2D>("Sheb");
+            myCats = new List<Texture2D> { binkaT, beanT, saraT, shebT };
+            introCatT = Content.Load<Texture2D>("MyCats");
+            myCatR = new Rectangle(0, 0, 50, 50);
+            catsR = new Rectangle(0, 0, gPW, gPH);
             // TODO: use this.Content to load your game content here
+            font = Content.Load<SpriteFont>("File");
+            v=new Vector2(gPW/4, gPH / 2);
+            screen = Screen.Intro;
         }
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
+            if (screen == Screen.Intro)
+                if (mouseState.LeftButton == ButtonState.Pressed)
+                    screen = Screen.Middle;
+                else;
+            else if (screen == Screen.Middle)
+                if (mouseState.RightButton == ButtonState.Pressed)
+                    screen = Screen.End;
+                else;
+            else if (screen == Screen.End)
+                if (mouseState.LeftButton == ButtonState.Pressed)
+                    this.Exit();
+                else;
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -50,7 +87,23 @@ namespace summative
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-
+            _spriteBatch.Begin();
+            if (screen == Screen.Intro)
+            {
+                _spriteBatch.Draw(introCatT, catsR, Color.White);
+                _spriteBatch.DrawString(font, "Left Click Right Left Click",v,Color.White);
+            }
+            else if (screen == Screen.Middle)
+            {
+                foreach (var cat in myCats)
+                    _spriteBatch.Draw(cat, myCatR, Color.White);
+            }
+            else if (screen == Screen.End)
+            {
+                _spriteBatch.Draw(introCatT, catsR, Color.White);
+                _spriteBatch.DrawString(font, "The End", v, Color.Blue);
+            }
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
